c33e2b1c0e8e32fc249e2f1754e464bb6dea0e5f
nyxlost <zeduffee@gmail.com>
2020-10-12 22:35:49 +0700

    button jong

--- navbar.php
+++ navbar.php
@@ -7,7 +7,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">สินค้า <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="index.php">สินค้า <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">ติดต่อ</a>
--- product_detail.php
+++ product_detail.php
@@ -10,7 +10,7 @@ $p_id = $_GET["id"];
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <!-- Bootstrap CSS -->
      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-
+      <link rel="stylesheet" href="css/style.css">
      <style>
         div.polaroid {
           width: 80%;
@@ -65,6 +65,7 @@ $p_id = $_GET["id"];
       </div>
     </div>
   </div>
+  <button class="btn-1">จองสินค้า</button>
 </body>
 </html>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
